e7e07eca05bd2fc9be316aa0a2cfae7b20ccb7ac
Justin D. Cotton <justin.d.cotton.esq@gmail.com>
2019-04-13 00:14:01 -0400

    Adding 'Hit' button logic

--- app.js
+++ app.js
@@ -155,7 +155,8 @@ let deckOfCards = [
 const getBetAmountFromPlayer = () => {
     currentBet = parseInt($("#bet").val())
     if (currentBet > playerBalance) {
-        alert("Insufficient Funds!");
+        // alert("Insufficient Funds!");
+        $("#headline").text("BUST! Player Loses").css("background", "red"
         $("#bet").val("");
         // $('.footer').show(0).delay(3000).hide(0);
     } else {
@@ -265,7 +266,7 @@ const hitPlayer = (hand) => {
     let cardFace = "url(" + playerHand[cardSlot].face + ")";
     $(newCard).css({"background": cardFace, "background-size": "contain"}).show();
     if (parseInt($("#playerScore").text) > 21) {
-        $("#headline").text("BUST! Player Loses").css("background", "red").show();
+        $("#headline").text("BUST! Player Loses").css("background", "red");
         $("#deal").show();
         $("#hit").hide();
         $("#stand").hide();
