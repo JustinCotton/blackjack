1bd2f2ab1ce1e6f152cc260be395651ea2a6606f
Justin D. Cotton <justin.d.cotton.esq@gmail.com>
2019-04-13 23:33:19 -0400

    Fixing bug allowing Player to play after betting more than she has

--- app.js
+++ app.js
@@ -158,7 +158,6 @@ const getBetAmountFromPlayer = () => {
         alert("Insufficient Funds!");
         // $("#headline").text("Insufficient Funds!").css("background", "red");
         $("#bet").val("100");
-        return;
         // $('.footer').show(0).delay(3000).hide(0);
     } else {
         playerBalance = playerBalance - currentBet;
