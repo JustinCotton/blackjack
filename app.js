a064ed2465646d6bc99a22719182ee0df34b8adf
Justin D. Cotton <justin.d.cotton.esq@gmail.com>
2019-04-13 22:34:27 -0400

    Fixing dealer's facedown card CSS

--- app.js
+++ app.js
@@ -458,21 +458,30 @@ $(function() {
     });
 
     $(".deck1").click(function () {
-        cardBack = "url('./deck1.jpg')";
+        // cardBack = "url('./deck3.jpg')"
+        $("#dealerCard0").addClass("deck1")
+        $("#dealerCard0").removeClass("deck2")
+        $("#dealerCard0").removeClass("deck3")
         $(".deck1").addClass("selected")
         $(".deck2").removeClass("selected")
         $(".deck3").removeClass("selected")
     });
 
     $(".deck2").click(function () {
-        cardBack = "url('./deck2.jpg')"
+        // cardBack = "url('./deck3.jpg')"
+        $("#dealerCard0").removeClass("deck1")
+        $("#dealerCard0").addClass("deck2")
+        $("#dealerCard0").removeClass("deck3")
         $(".deck1").removeClass("selected")
         $(".deck2").addClass("selected")
         $(".deck3").removeClass("selected")
     });
 
     $(".deck3").click(function () {
-        cardBack = "url('./deck3.jpg')"
+        // cardBack = "url('./deck3.jpg')"
+        $("#dealerCard0").removeClass("deck1")
+        $("#dealerCard0").removeClass("deck2")
+        $("#dealerCard0").addClass("deck3")
         $(".deck1").removeClass("selected")
         $(".deck2").removeClass("selected")
         $(".deck3").addClass("selected")
